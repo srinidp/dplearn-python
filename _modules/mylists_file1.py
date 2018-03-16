c126108729507e682416674365be5f9999afcaeb
srinidp <pen2srini@gmail.com>
2018-03-16 20:48:44 +0530

    added slice and comprehension
    added slice and comprehension

--- _modules/mylists_file1.py
+++ _modules/mylists_file1.py
@@ -14,8 +14,8 @@
 # len(lst)
 # lst[index]
 # lst[-index]   => -1 is last item
-# lst[i:j];lst[i:];lst[:j]
-#
+# lst[i:j];lst[i:];lst[:j]  => j is excluded
+# lst[start:end:step]   => end is excluded
 # emptylst = []  or emptylst = list()
 #
 # data in lst
@@ -38,13 +38,17 @@
 # lst.pop() => removes and returns last element
 # lst.reverse
 # lst.sort();lst.sort(reverse=True)
-# sorted(lst) => returns a newly sorted list
+# lst.index(data)
+#
+# sorted(lst);sorted(lst,reverse=True)  => returns a newly sorted list
+# sorted(lst,key=abs)   => returns a newly sorted list as per abs value
 # min(lst)
 # max(lst)
 # sum(lst)
-# lst.index(data)
 # str.split('x')    => retuns a new list after splitting str at each occurrence of x
 # 'x'.join(lst)     => retuns a new str after joining all elements of lst using x
+#
+# list comprehension => use []
 #
 # Created:     16/03/2018
 # Copyright:   (c) srini_000 2018
@@ -58,19 +62,57 @@ def mystring2list():
     print(mystr)
     print(mylst)
 
+def mylistslice():
+    print("***mylistslice***")
+
+    mylst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    #index   0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    #-index-10,-9,-8,-7,-6,-5,-4,-3,-2,-1
+
+    print("     3  -  8:", mylst[3:9])
+    print("     3 - end:", mylst[3:])
+    print("     odd nos:", mylst[1::2])
+    print("     rev nos:", mylst[::-1])
+    print(" rev odd nos:", mylst[-1:-10:-2])
+
+def mylistcomprehension():
+    print("***mylistcomprehension***")
+    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    # i want n for each n in nums
+    mylst = [n for n in nums]
+    print("my list:", mylst)
+
+    # i want n*n for each n in nums
+    mylst = [n*n for n in nums]
+    print("my list:", mylst)
+
+    # i want n for each n in nums if n is even
+    mylst = [n for n in nums if n%2==0]
+    print("my list:", mylst)
+
+    # i want a (letter,num)pair n for each letter in 'abcd'
+    # and each number in '0123' => list of tuples
+    letters = ['a','b','c','d']
+    nums = [0, 1, 2, 3]
+    mylst = [(letter,num) for letter in letters for num in nums]
+    print("my list:", mylst)
+
 def listsmain():
 
     print("lists")
 
     mystring2list()
     print()
+    mylistslice()
+    print()
+    mylistcomprehension()
+    print()
 
     print("lists end")
 
 listsmain()
 
-##Help on class list in module builtins:
-##
 ##class list(object)
 ## |  list() -> new empty list
 ## |  list(iterable) -> new list initialized from iterable's items
