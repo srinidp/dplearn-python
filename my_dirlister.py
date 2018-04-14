5a3d2bd0a0c7597dbe774408a1bebff97fad6b00
srinidp <36436922+srinidp@users.noreply.github.com>
2018-04-14 15:20:34 +0530

    updated with io handles

--- my_dirlister.py
+++ my_dirlister.py
@@ -1,71 +1,123 @@
 import os
 import csv
 
-srcpath = r'D:\Users\srinivasandp\Downloads\srinivasan\dropit\Dropbox'
-size_mb = 1024*1024
-
-print("start dir lister")
-
-if (os.path.isdir(srcpath)):
-    print('path exists')
-else:
-    print('path does not exists')
-    exit
-
+##global
 fsize = {}
-for cfolder,sfolders,filenames in os.walk(srcpath):
-    fsize.setdefault(cfolder,0)
-    #print(cfolder)
 
-for cfolder,sfolders,filenames in os.walk(srcpath):
-    foldersize = 0
+##process
+def dirlister_p(srcpath):
+    size_mb = 1024*1024
 
-    for files in os.listdir(cfolder):
-        foldersize = foldersize + os.path.getsize(os.path.join(cfolder,files))
+    for cfolder,sfolders,filenames in os.walk(srcpath):
+        fsize.setdefault(cfolder,0)
+        #print(cfolder)
 
-    foldersize = foldersize/size_mb
+    for cfolder,sfolders,filenames in os.walk(srcpath):
+        foldersize = 0
+        for files in os.listdir(cfolder):
+            foldersize = foldersize + os.path.getsize(os.path.join(cfolder,files))
+        foldersize = foldersize/size_mb
+        #print("cf:{}".format(cfolder))
+        for keys in fsize:
+            if keys in cfolder:
+                #print("=> key:{}".format(keys))
+                fsize[keys] += foldersize
 
-    print()
-    #print("cf:{}".format(cfolder))
-    for keys in fsize:
-        if keys in cfolder:
-            #print("=> key:{}".format(keys))
-            fsize[keys] += foldersize
+##output
+def dirlister_o(srcpath, txtpath, csvpath):
 
-cwdpath = os.getcwd()
+    txtfile = open(txtpath, 'a')
+    csvfile = open(csvpath,'a',newline='')
+    csvwriter = csv.writer(csvfile)
 
-wpath = os.path.join(cwdpath,"dirlist.txt")
-wfile = open(wpath, 'w')
-wfile.write('Dir List\n')
-wfile.close()
+    for cfolder,sfolders,filenames in os.walk(srcpath):
+        text = cfolder.replace(srcpath,'')
+        text = text.replace('\\',',')
+        #print(text)
+        #print("{},{:.2f}{}".format(cfolder,fsize[cfolder],text))
 
-csvpath = os.path.join(cwdpath,"dirlist.csv")
-csvfile = open(csvpath,'w',newline='')
-csvwriter = csv.writer(csvfile)
-header = ['path','size']
-csvwriter.writerow(header)
+        ##text file write
+        data = "{},{:.2f}{}\n".format(cfolder,fsize[cfolder],text)
+        #print(data)
+        txtfile.write(data)
 
-wfile = open(wpath, 'a')
+        ##csv file write
+        data = "{},{:.2f}{}".format(cfolder,fsize[cfolder],text)
+        #print(data)
+        csvdata = data.split(',')
+        #print(csvdata)
+        csvwriter.writerow(csvdata)
 
-for cfolder,sfolders,filenames in os.walk(srcpath):
-    text = cfolder.replace(srcpath,'')
-    text = text.replace('\\',',')
-    #print(text)
-    #print("{},{:.2f}{}".format(cfolder,fsize[cfolder],text))
+    txtfile.close()
+    csvfile.close()
 
-    ##text file write
-    data = "{},{:.2f}{}\n".format(cfolder,fsize[cfolder],text)
-    #print(data)
-    wfile.write(data)
+def pathcheck(path, ptype):
+    status = False
+    if(ptype == 'f'):
+        if(os.path.isfile(path)):
+           print('file exists')
+           status = True
+        else:
+            print('no file exists => {}'.format(path))
+    else:
+        if (os.path.isdir(path)):
+            print('path exists')
+            status = True
+        else:
+            print('no path exists => {}'.format(path))
+    return status
 
-    ##csv file write
-    data = "{},{:.2f}{}".format(cfolder,fsize[cfolder],text)
-    #print(data)
-    csvdata = data.split(',')
-    #print(csvdata)
-    csvwriter.writerow(csvdata)
+##input
+def dirlister_i():
+    iofile = open('_io_dirlister.csv','r')
+    csv_reader = csv.DictReader(iofile)
+    outputlist = []
 
-wfile.close()
-csvfile.close()
+    for line in csv_reader:
+        #print(line)
+        src = line['input path']
+        dst = line['output path']
+        ##check for input path
+        if(pathcheck(src,'d') == False):
+            continue;
+        ##check for empty output path
+        if dst == '':
+            dst = os.getcwd()
+        ##check for empty output file
+        if line['output file'] == '':
+            line['output file'] = 'o_dirlister'
+        dst_text = dst + '\\' + line['output file'] + '.txt'
+        dst_csv = dst + '\\' + line['output file'] + '.csv'
+        print(src)
+        print(dst_text)
+        print(dst_csv)
 
-print("end dir lister")
+        ##process
+        dirlister_p(src)
+
+        if dst_text not in outputlist:
+            outputlist.append(dst_text)
+            ##pre-condition text
+            txtfile = open(dst_text, 'w')
+            txtfile.write('dir lister\n')
+            txtfile.close()
+            ##pre-condition csv
+            csvfile = open(dst_csv,'w',newline='')
+            csvwriter = csv.writer(csvfile)
+            header = ['path','size']
+            csvwriter.writerow(header)
+            csvfile.close()
+
+        ##output
+        dirlister_o(src,dst_text, dst_csv)
+
+##main
+def dirlistermain():
+    print("start dir lister")
+
+    dirlister_i()
+
+    print("end dir lister")
+
+dirlistermain()
+
